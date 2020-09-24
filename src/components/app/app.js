56ca49307b9f1d8ba12d76d5354c55c4b90ed883
Gelfande <tala.gelfande@accenture.com>
2020-09-24 18:36:32 +0300

    created sign in error page and the route for it

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import SignInPage from '../pages/signInPage';
 import SignUpPage from '../pages/signUpPage';
+import SignInErrorPage from '../pages/signInErrorPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         {/* <Route path="/" component={SignInPage}/> */}
+        <Route exact path="/signinerror" component={SignInErrorPage}/>
         <Route exact path="/signin" component={SignInPage}/>
         <Route exact path="/signup" component={SignUpPage}/>
       </div>
