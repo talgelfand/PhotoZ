0ce6be779ebf64763f100c6e0ca2066400ae6ba3
DIR\tala.gelfande <tala.gelfande@accenture.com>
2020-09-29 22:07:39 +0300

    added variables for colors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.scss';
+import './styles/colors.scss';
 import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
