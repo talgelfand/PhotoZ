85f611d905eae2b4f4b3816e9b4101e34ec9960c
DIR\tala.gelfande <tala.gelfande@accenture.com>
2020-09-29 22:41:46 +0300

    started working on post component

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import NavBar from '../../components/navBar';
+import Post from '../../components/post';
+
+import './mainPage.scss';
 
 function MainPage() {
     return (
-        <NavBar/>
+        <div className="main">
+            <NavBar/>
+            <Post/>
+        </div>
     )
 }
 
