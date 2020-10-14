85ff0643355cba103dd1ddb20ef022ca40444026
DIR\tala.gelfande <tala.gelfande@accenture.com>
2020-10-14 12:52:43 +0300

    created different toaster types instead of modal component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import SignInPage from '../../pages/signInPage';
-import SignUpPage from '../../pages/signUpPage';
-import SignInErrorPage from '../../pages/signInErrorPage';
-import MainPage from '../../pages/mainPage';
-import ProtectedRoute from '../protectedRoute';
-import Auth from '../auth';
+import SignInPage from '../../pages/signInPage'
+import SignUpPage from '../../pages/signUpPage'
+import SignInErrorPage from '../../pages/signInErrorPage'
+import MainPage from '../../pages/mainPage'
+import ProtectedRoute from '../protectedRoute'
+import Auth from '../auth'
 
 function App() {
-
   return (
-      <Router>
-        <Auth>
-          <Switch>
-            <div className="App">
-              <Route exact path={["/", "/signin"]} component={SignInPage}/>
-              <Route exact path="/signinerror" component={SignInErrorPage}/>
-              <Route exact path="/signup" component={SignUpPage}/>
-              <ProtectedRoute exact path="/main" component={MainPage}/>
-            </div>
-          </Switch>
-        </Auth>
-      </Router>
-  );
+    <Router>
+      <Auth>
+        <Switch>
+          <div className="App">
+            <Route exact path={['/', '/signin']} component={SignInPage} />
+            <Route exact path="/signinerror" component={SignInErrorPage} />
+            <Route exact path="/signup" component={SignUpPage} />
+            <ProtectedRoute exact path="/main" component={MainPage} />
+          </div>
+        </Switch>
+      </Auth>
+    </Router>
+  )
 }
 
-export default connect()(App);
+export default connect()(App)
--- src/pages/signInPage/signInPage.js
+++ src/pages/signInPage/signInPage.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import SignInForm from '../../components/forms/signInForm';
-import Logo from '../../components/logo';
-import FlatLayout from '../../layouts/flatLayout';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SignInForm from '../../components/forms/signInForm'
+import Logo from '../../components/logo'
+import FlatLayout from '../../layouts/flatLayout'
+import MyToast from '../../utils/toaster'
 
-import './signInPage.scss';
+import './signInPage.scss'
 
 function SignInPage() {
-    return (
-        <FlatLayout>
-            <Logo/>
-            <SignInForm/>
-            <Link to="/signup" className="signup" color="text-primary">Sign up</Link>
-        </FlatLayout>
-    )
+  return (
+    <FlatLayout>
+      <Logo />
+      <SignInForm />
+      <Link to="/signup" className="signup" color="text-primary">
+        Sign up
+      </Link>
+      <MyToast />
+    </FlatLayout>
+  )
 }
 
-export default SignInPage;
+export default SignInPage
